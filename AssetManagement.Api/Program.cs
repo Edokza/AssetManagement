fb4518b9bd6e5b41eecb2333c957b72e379cfea8
EdokZa <zlyzoey@gmail.com>
2026-02-03 04:47:08 +0700

    Connect DB

--- AssetManagement.Api/Program.cs
+++ AssetManagement.Api/Program.cs
@@ -1,7 +1,15 @@
+using AssetManagement.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
