10a9000df372a601435c917cfc943f3bbab2d43e
EdokZa <zlyzoey@gmail.com>
2026-02-03 04:35:48 +0700

    Add model

---
+++ AssetManagement.Api/Models/AssetModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssetManagement.Api.Models
+{
+    public class AssetModel
+    {
+        public int AssetId { get; set; }
+        [Required]
+        public string AssetName { get; set; } = null!;
+        public string AssetTag { get; set; } = null!;
+        public int CategoryId { get; set; }
+    }
+}
---
+++ AssetManagement.Api/Models/CategoryModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssetManagement.Api.Models
+{
+    public class CategoryModel
+    {
+        public int CategoryId { get; set; }
+        [Required]
+        public string CategoryName { get; set; } = null!;
+    }
+}
