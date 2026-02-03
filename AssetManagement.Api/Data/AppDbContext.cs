1ed8ce1e177f2ff45e99fbe607d41945cf1f9571
phnut <phnut@MSI>
2026-02-04 00:36:30 +0700

    fix port and mock Data for Categery

--- AssetManagement.Api/Data/AppDbContext.cs
+++ AssetManagement.Api/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AssetManagement.Api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AssetManagement.Api.Data
 {
@@ -7,7 +8,21 @@ namespace AssetManagement.Api.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-        public DbSet<Models.AssetModel> Assets { get; set; } = null!;
-        public DbSet<Models.CategoryModel> Categories { get; set; } = null!;
+        public DbSet<AssetModel> Assets { get; set; } = null!;
+        public DbSet<CategoryModel> Categories { get; set; } = null!;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CategoryModel>().HasData(
+                new CategoryModel { CategoryId = 1, CategoryName = "Laptop" },
+                new CategoryModel { CategoryId = 2, CategoryName = "Monitor" },
+                new CategoryModel { CategoryId = 3, CategoryName = "Keyboard" },
+                new CategoryModel { CategoryId = 4, CategoryName = "Mouse" },
+                new CategoryModel { CategoryId = 5, CategoryName = "Headphone" }
+            );
+        }
     }
 }
---
+++ AssetManagement.Api/Migrations/20260203173441_SeedCategory.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace AssetManagement.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName" },
+                values: new object[,]
+                {
+                    { 1, "Laptop" },
+                    { 2, "Monitor" },
+                    { 3, "Keyboard" },
+                    { 4, "Mouse" },
+                    { 5, "Headphone" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 5);
+        }
+    }
+}
