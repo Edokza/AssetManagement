979a775da61a6da86c707f38ac79468b4b785818
EdokZa <zlyzoey@gmail.com>
2026-02-03 19:14:36 +0700

    update Dto

--- AssetManagement.Api/DTOs/AssestDto.cs
+++ AssetManagement.Api/DTOs/AssestDto.cs
@@ -5,9 +5,12 @@ namespace AssetManagement.Api.DTOs
     public class AssetCreateDto
     {
         [Required]
+        [StringLength(100, MinimumLength = 3,ErrorMessage = "Asset name must be between 3 and 100 characters")]
+        [RegularExpression(@".*\S+.*", ErrorMessage = "Asset name cannot be empty or whitespace")]
         public string AssetName { get; set; }
 
         [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "CategoryId must be greater than 0")]
         public int CategoryId { get; set; }
     }
 
--- AssetManagement.Api/DTOs/CategoryDTO.cs
+++ AssetManagement.Api/DTOs/CategoryDTO.cs
@@ -5,6 +5,8 @@ namespace AssetManagement.Api.DTOs
     public class CategoryCreateDto
     {
         [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Category name must be between 2 and 50 characters")]
+        [RegularExpression(@".*\S+.*", ErrorMessage = "Category name cannot be empty or whitespace")]
         public string CategoryName { get; set; }
     }
     public class CategoryReadDto
